3663ae0a0ed0c455769157760b2dd460d7a2620d
Reketino <bjornevensk8@gmail.com>
2026-09-23 00:04:30 +0200

    style: refactor updateAdBlocking function for clarity and consistency; add ads.json ruleset file

--- src/background/index.ts
+++ src/background/index.ts
@@ -4,46 +4,44 @@ import { debug } from "../utils/debug";
 const RULESET_ID = "ads";
 
 async function updateAdBlocking(): Promise<void> {
-    const settings = await getSettings();
+  const settings = await getSettings();
 
-    if (settings.blockAds) {
-        await chrome.declarativeNetRequest.updateEnabledRulesets({
-            enableRulesetIds: [RULESET_ID],
-            disableRulesetIds: [],
-        });
-
-        debug("Ad blocking enabled");
-        return;
-    }
-
+  if (settings.blockAds) {
     await chrome.declarativeNetRequest.updateEnabledRulesets({
-        enableRulesetIds: [],
-        disableRulesetIds: [RULESET_ID],
+      enableRulesetIds: [RULESET_ID],
+      disableRulesetIds: [],
     });
 
-    debug("Ad blocking disabled")
+    debug("Ad blocking enabled");
+    return;
+  }
+
+  await chrome.declarativeNetRequest.updateEnabledRulesets({
+    enableRulesetIds: [],
+    disableRulesetIds: [RULESET_ID],
+  });
+
+  debug("Ad blocking disabled");
 }
 
 async function initialize(): Promise<void> {
-    debug("🐻 BearTube background initializing")
+  debug("🐻 BearTube background initializing");
 
-    await updateAdBlocking();
+  await updateAdBlocking();
 
-    chrome.storage.onChanged.addListener(async (changes, areaName)) => {
-        if (areaName !== "sync") {
-            return;
-        }
-
-        if (!changes.blockAds) {
-            return;
-        }
-
-        debug("Block Ads setting changed");
+  chrome.storage.onChanged.addListener(async (changes, areaName) => {
+    if (areaName !== "sync") {
+      return;
     }
 
-    const settings = await getSettings();
+    if (!changes.blockAds) {
+      return;
+    }
 
-    debug("Background settings.", settings)
+    debug("Block Ads setting changed");
+
+    await updateAdBlocking();
+  });
 }
 
 void initialize();
